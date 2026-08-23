78746c4bc64115c9534104ea13bb88d793d053d3
Dhananjay Kisan Ramteke <dhan@Dhananjays-MacBook-Pro.local>
2026-08-22 23:24:52 -0600

    Fix Prisma 7 client generation and driver adapter setup

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
 import { NextResponse } from "next/server";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
