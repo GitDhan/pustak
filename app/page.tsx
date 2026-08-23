5323b63e5d3cd8f1a2567acb992d1b41990a6e89
Dhananjay Kisan Ramteke <dhan@Dhananjays-MacBook-Pro.local>
2026-08-22 23:45:40 -0600

    replaced local api call to prisma call

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
-import Image from "next/image";
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-async function getBooks() {
-  const res = await fetch("http://localhost:3000/api/books", {
-    cache: "no-store",
-  });
-  return res.json();
-}
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default async function Home() {
-  const books = await getBooks();
+  const books = await prisma.book.findMany({
+    orderBy: { createdAt: "desc" },
+  });
 
-   return (
+  return (
     <main className="min-h-screen bg-neutral-950 text-white px-8 py-12">
       <h1 className="text-3xl font-bold mb-8">Pustak</h1>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8">
-        {books.map((book: any) => (
+        {books.map((book) => (
           <div key={book.id} className="cursor-pointer group">
             <img
               src={book.coverImageUrl}
